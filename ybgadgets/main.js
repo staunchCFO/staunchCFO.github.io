e3a964b8dc8adcc20ae7713fa46b96353a958c53
staunchCFO <staunchCFO@users.noreply.github.com>
2019-12-31 20:52:19 +0100

    new yb

--- ybgadgets/main.js
+++ ybgadgets/main.js
@@ -115,3 +115,15 @@
 	});
 
 })(jQuery);
+
+const portfolioItems = document.querySelectorAll('.portfolio-item')
+
+portfolioItems.forEach(portfolioItem => {
+	portfolioItem.addEventListener('mouseover', () => {
+		portfolioItem.childNodes[1].classList.add('img-darken');
+	})
+	portfolioItem.addEventListener('mouseout', () => {
+		portfolioItem.childNodes[1].classList.remove('img-darken');
+	})
+	
+})
